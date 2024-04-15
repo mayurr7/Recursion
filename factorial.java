25a8293e4bc6272f7b9df3bcdde4958396b86c1f
Mayur Tekale <147367560+mayurr7@users.noreply.github.com>
2024-04-15 13:38:11 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class factorial {
+    static int factorialN(int n){
+        if(n == 0) return 1;
+
+        int fact = factorialN(n - 1);
+
+        return n * fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n =  sc.nextInt();
+
+        System.out.println("answe is: "+ factorialN(n));
+    }
+}38.23
